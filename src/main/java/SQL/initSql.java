8821b7ae922377fa6cce499d6f6830cab100f3cf
yangwenjun <yangwjy@yonyou.com>
2022-12-11 10:45:09 +0800

    SQL

---
+++ src/main/java/SQL/initSql.java
@@ -0,0 +1,13 @@
+package SQL;
+
+/**
+ * Created by yangwenjun on 2022/12/11 10:42
+ */
+public class initSql {
+
+  /**
+   * 1.使用初始化程序将数据load进去
+   * 2.sql ,借助 函数、一对多(和返回字段有关 - mybatis映射)、行转列 、 --> 图
+   */
+
+}
